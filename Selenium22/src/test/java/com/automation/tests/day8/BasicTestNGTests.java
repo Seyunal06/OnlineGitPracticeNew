e053f84d987bae8170f064bd57868443ddffe05d
Seyma Unal <seyma07u@gmail.com>
2023-03-29 17:52:14 -0400

    added column names for users.csv file

---
+++ Selenium22/src/test/java/com/automation/tests/day8/BasicTestNGTests.java
@@ -0,0 +1,11 @@
+package com.automation.tests.day8;
+
+import org.testng.Assert;
+
+public class BasicTestNGTests {
+    public void test1(){
+        String expected ="apple";
+        String actual ="apple";
+        Assert.assertEquals(actual, expected);
+    }
+}
